57d45d7ee52eb218d3154cbae8d3b806ed10bfc4
Adam Ovadia <adam@aovadia.com>
2024-06-05 18:25:42 -0400

    Upload progress

--- todo.py
+++ todo.py
@@ -1,4 +1,6 @@
-# individual task in ToDoList class
+import datetime as dt
+
+# individual Task
 class Task:
     task_id = 1
 
@@ -6,8 +8,18 @@ class Task:
         self.description = description
         self.deadline = deadline
         self.status = status
-        self.priority = priority
+        
+        # convert priority to int values
+        if priority.lower() == "low":
+            self.priority = 3
+        elif priority.lower() == "medium":
+            self.priority = 2
+        elif priority.lower() == "high":
+            self.priority = 1
+
+        # Set Task ID
         self.id = Task.task_id
+        # Incrament Task ID
         Task.task_id += 1
     
     def set_description(self, desc):
@@ -24,27 +36,61 @@ class Task:
 
     def get_task_id(self): return self.id
 
-class ToDoList:
+    def change_status(self, new_status):
+        self.status = new_status
 
-    ToDo = []
+# holds the actual data -> List of Tasks
+class ToDoList: 
 
-    def __init__(self): print("h")
+    def __init__(self):
+        self.tasks = []
 
     # Add a new task
     def add_task(self, new_task : Task):
-        self.ToDo.append(new_task)
+        self.tasks.append(new_task)
 
     # Remove task from list
-    def remove_task(self, remove_task : Task): print("hg")
+    def remove_task(self, remove_task_id : int):
+        for task in self.tasks:
+            if task.id == remove_task_id:
+                self.tasks.remove(task)
+        """
+            Need to handle errors
+        """
+
+    def view_tasks(self):
+        return
 
 
-
-
+# Menu for user management of ToDoList
+# Is a user interface to interact with the ToDoList
 class UserInterface:
 
-    def __init__():
-        print("Main Menu")
+    def __init__(self, todo_list : ToDoList):
+        self.todo_list = todo_list
+
+    def add_new_task(self):
+        task_desciption = input("Enter task Description\n")
+        deadline_str = input("Enter deadline in (mm/dd/yyyy hh:mm) format: ")
+        # convert deadline_str into a datetime format
+        task_status = input("Enter status (not started or in progress): ")
+        task_priority = input("Enter priority (low or medium or high)")
+        # Create new Task obj
+        new_task = Task(task_desciption, deadline_str, task_status, task_priority)
+        # Add Task to ToDoList
+        self.todo_list.add_task(new_task)
+        self.view_tasks()
 
     
+
+
     def display_main_menu(self):
-        print("1. View all tasks")
+        print("1. Add a new Task")
+        print("2. View all Tasks")
+
+        if input() == "2":
+            self.todo_list.view_tasks()
+
+my_list = ToDoList()
+ui = UserInterface(my_list)
+ui.display_main_menu()
