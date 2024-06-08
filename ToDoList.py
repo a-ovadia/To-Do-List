1706f546193ac0f564e6af507f88c0071f58c16b
Adam Ovadia <adam@aovadia.com>
2024-06-08 11:02:16 -0400

    Remove unused functions and vars

--- ToDoList.py
+++ ToDoList.py
@@ -5,10 +5,8 @@ import Task
 class ToDoList: 
     # Constructor
     def __init__(self, csv_obj : CSVHandler):
-        self.tasks = []
-        self.file_path = csv_obj.path
         self.csv_obj = csv_obj
-        self.task = None
+     
     """
     Represents a collection of Tasks objects
     """
@@ -40,15 +38,6 @@ class ToDoList:
         """
         self.csv_obj.print_csv_file()
 
-    def validate_task_id(self, task_id):
-        """
-        Check if an ID is in the Tasks list
-        Args:
-        task_id -- (int) return true if task_id matches an element in the Tasks list, False otherwise
-        """
- 
-        return False   
-
     
     def update_task(self, update_task, task_id):
 
@@ -61,5 +50,4 @@ class ToDoList:
         update_status -- (str) new task status
         update_priority -- (str) new task priority
         """   
-        
         self.csv_obj.update_csv_file(update_task, task_id)
