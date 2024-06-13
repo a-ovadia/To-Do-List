ddcf112806113f4d9c84149e9ef5c3568fb4ba0a
Adam Ovadia <adam@aovadia.com>
2024-06-13 17:41:38 -0400

    Implement the rest of the sorted methods

--- Sorted.py
+++ Sorted.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 # Class used to provide sorting capabilities
 class Sorter:
 
@@ -19,9 +19,14 @@ class Sorter:
         return self.data
     
     def priority_key(self, item):
-        """Custom key for sorting by priority."""
+        """Custom key for sorting by priority"""
         priority_map = {'high': 1, 'medium': 2, 'low': 3}
         return priority_map.get(item[1].lower()) 
+    
+    def status_key(self, item):
+        """Custom key for sorting by status"""
+        status_map = {"not started": 1, "in progress": 2, "completed": 3}
+        return status_map.get(item[3].lower())
 
     """
 1. Sort by Priority (Ascending)
@@ -37,7 +42,31 @@ class Sorter:
 """
     def sort(self):
         if self.option == "1":
-            return sorted(self.data, key=self.priority_key)
+            return sorted(self.data, key = self.priority_key)
 
         elif self.option == "2":
-            return sorted(self.data, key=self.priority_key, reverse=True)
+            return sorted(self.data, key = self.priority_key, reverse = True)
+         
+        elif self.option == "3":
+            return sorted(self.data, key = lambda x: x[2])
+
+        elif self.option == "4":
+            return sorted(self.data, key = lambda x: x[2], reverse = True)
+        
+        elif self.option == "5":
+            return sorted(self.data, key = self.status_key)
+        
+        elif self.option == "6":
+            return sorted(self.data, key = self.status_key, reverse = True)
+        
+        elif self.option == "7":
+            return sorted(self.data, key = lambda x: datetime.strptime(x[4], "%d/%m/%Y %H:%M"))
+        
+        elif self.option == "8":
+            return sorted(self.data, key = lambda x: datetime.strptime(x[4], "%d/%m/%Y %H:%M"), reverse = True)        
+
+        elif self.option == "9":
+            return sorted(self.data, key = lambda x: datetime.strptime(x[5], "%d/%m/%Y %H:%M"))
+        
+        elif self.option == "10":
+            return sorted(self.data, key = lambda x: datetime.strptime(x[5], "%d/%m/%Y %H:%M"), reverse = True)        
